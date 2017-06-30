01a11693b22ae14c8b22c847eaa30b90e86ae790
akiyama@sssagi.net <akiyama@sssagi.net>
2017-06-30 15:30:46 +0900

    remove useless property

--- response.js
+++ response.js
@@ -9,13 +9,28 @@ class response {
 				type :  Date
 			},
 		},data);
-		/*
 		if(this.tradeReduced){
-			console.log("this.tradeReduced",this.tradeReduced);
-			this.tradeReduced = new _trade(this.tradeReduced,api,account);
-		}*/
+			if(Object.keys(this.tradeReduced).length == 0){
+				delete this.tradeReduced;
+			}else{
+				this.tradeReduced = new _trade(this.tradeReduced,api,account);
+			}
+		}
+		if(this.tradesClosed){
+			if(this.tradesClosed.length == 0){
+				delete this.tradesClosed;
+			}else{
+				this.tradesClosed = this.tradesClosed.map(function(single){
+					return new _trade(single,api,account);
+				});
+			}
+		}
 		if(this.tradeOpened){
-			this.tradeOpened = new _trade(this.tradeOpened,api,account);
+			if(Object.keys(this.tradeOpened).length == 0){
+				delete this.tradeOpened;
+			}else{
+				this.tradeOpened = new _trade(this.tradeOpened,api,account);
+			}
 		}
 		if(this.orderOpened){
 			this.orderOpened = new _order(this.orderOpened,api,account);
